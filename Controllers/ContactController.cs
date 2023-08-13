f23d7b614c164149a1426dc1914e3af66dfebd58
itani99 <itani0369-@hotmail.com>
2023-08-13 04:33:30 +0300

    Add project files.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Mail;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using UmbracoProject.Models;
+using UmbracoProject.Utilities;
+
+namespace UmbracoProject.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        public ContactController(IUmbracoContextAccessor umbracoContextAccessor,
+            IUmbracoDatabaseFactory databaseFactory,
+            ServiceContext services,
+            AppCaches appCaches,
+            IProfilingLogger profilingLogger,
+            IPublishedUrlProvider publishedUrlProvider)
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        { }
+
+      
+        [HttpPost]
+        public IActionResult Submit(EmailForm model)
+        {
+           
+            MailSender.SendEmail(model);
+
+            return RedirectToCurrentUmbracoPage();
+        } 
+    }
+}
---
+++ Utilities/MailSender.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Umbraco.Cms.Infrastructure.Mail;
+using UmbracoProject.Models;
+
+namespace UmbracoProject.Utilities
+{
+    public class MailSender
+    {
+
+        public static void SendEmail(EmailForm mailModel)
+        {
+        string recipientEmail = mailModel.Receiever;
+
+
+
+            // Create a new MailMessage
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("admin@umbraco.com");
+            mail.To.Add(recipientEmail);
+            mail.Subject = mailModel.Title;
+            mail.Body = mailModel.Message;
+
+            // Create a new SmtpClient
+            SmtpClient smtpClient = new SmtpClient("127.0.0.1",25);
+
+            try
+            {
+                // Send the email
+                smtpClient.Send(mail);
+                Console.WriteLine("Email sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending email: {ex.Message}");
+            }
+            
+
+        }
+    }
+}
